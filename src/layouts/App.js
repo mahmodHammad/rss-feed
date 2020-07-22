6a1c1ffa35d4ccb3ef59a788458a236560afa33b
Mahmoud Hammad <hodaman2012@gmail.com>
2020-07-22 02:26:58 +0200

    fetch->parse->render the rss !!!

--- src/layouts/App.js
+++ src/layouts/App.js
@@ -1,11 +1,16 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Router, Route, Switch, Redirect } from "react-router-dom";
 import routes from "../routes.js";
 // @material-ui/core components
 import { createBrowserHistory } from "history";
-import  Footer  from "../components/Footer/Footer.js";
-import  Navbar  from "../components/Navbar/Navbar.js";
+import Footer from "../components/Footer/Footer.js";
+import Navbar from "../components/Navbar/Navbar.js";
+import RSSParser from "rss-parser";
 const hist = createBrowserHistory();
+
+let parser = new RSSParser();
+
+const CORS_PROXY = "https://cors-anywhere.herokuapp.com/";
 
 const switchRoutes = (
   <Router history={hist}>
@@ -19,12 +24,44 @@ const switchRoutes = (
 );
 
 // const useStyles = makeStyles(styles);
-
+function getfeed(url) {
+  return new Promise((resolve, reject) => {
+    parser.parseURL(CORS_PROXY + url, function (err, feed) {
+      if (err) throw err;
+      console.log(feed);
+      resolve(feed);
+    });
+  });
+}
 export default function Admin({ ...rest }) {
+  const [providers, setproviders] = useState([
+    "https://www.reddit.com/.rss",
+    "http://joeroganexp.joerogan.libsynpro.com/rss",
+  ]);
+  const [Feeds, setFeeds] = useState([]);
+  useEffect(() => {
+    getfeed(providers[0]).then((feeds) => {
+      let updatedFeeds = [...Feeds, feeds];
+
+      setFeeds(updatedFeeds);
+    });
+  });
+
   return (
     <div>
       <Navbar />
       <div>{switchRoutes}</div>
+      <ol>
+        {Feeds.length === 0
+          ? "Loading..."
+          : Feeds.map((f) =>
+              f.items.map((item) => (
+                <li>
+                  {item.title} : <a href={item.link}>Visit</a>
+                </li>
+              ))
+            )}
+      </ol>
       <Footer />
     </div>
   );
