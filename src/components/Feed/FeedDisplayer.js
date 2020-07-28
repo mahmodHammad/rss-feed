a8ac17f5098159663a07003d331dee386a4b6c79
mahmoud_Hammad <hodaman2012@gmail.com>
2020-07-29 01:01:26 +0200

    init pagination

--- src/components/Feed/FeedDisplayer.js
+++ src/components/Feed/FeedDisplayer.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 
-
 // Icons 
 import AccessTime from "@material-ui/icons/AccessTime";
 // core components
@@ -15,62 +14,35 @@ import CardBody from "../Card/CardBody.js";
 import CardFooter from "../Card/CardFooter.js";
 import styles from "../../assets/jss/components/FeedStyle"
 import moment from "moment"
+import Pagination from "../Pagination/Pagination"
 
 const useStyles = makeStyles(styles);
 export default function FeedDisplayer({ Feeds }) {
+  const [displayFeeds, setdisplayFeeds] = useState([])
+  const [count, setcount] = useState(10)
+
+
+  function handlePagination(e, v) {
+    let paginated = Feeds.slice(v * 25, v + 25)
+    setdisplayFeeds(paginated)
+    console.log("VVVVVVVVVVV", v)
+  }
   const classes = useStyles();
 
-  console.log("feeds", Feeds)
+  // console.log("feeds", Feeds)
 
   function getFromNow(time) {
     return moment(time).fromNow()
-
   }
+
   return (
-
     <GridContainer>
-      {Feeds.map((f) =>
-        f.items.map((item) => (
+      <Pagination count={count} handlePagination={handlePagination} />
+      {
 
-          <GridItem xs={12} sm={4} md={3} xl={2} component="a" href={item.link} target="_blank">
-            <Card chart>
-              <CardHeader color="info" >
-                {item.title}
-              </CardHeader>
-              <CardBody>
-                <h4 className={classes.cardTitle}>Daily Sales</h4>
-                <p className={classes.cardCategory} dangerouslySetInnerHTML={{ __html: item.contentSnippet || item.contentSnippet }} >
-
-                </p>
-              </CardBody>
-              <CardFooter chart>
-                <div className={classes.stats}>
-                  <AccessTime /> Published {getFromNow(item.pubDate)}
-                </div>
-              </CardFooter>
-            </Card>
-          </GridItem>
-
-        )))
+        console.log(displayFeeds)
       }
 
     </GridContainer>
-
-
-
-
-
-    // <ol>
-    //   {Feeds.length === 0
-    //     ? "Loading..."
-    //     : Feeds.map((f) =>
-    //       f.items.map((item) => (
-    //         <li>
-    //           {item.title} : <a href={item.link}>Visit</a>
-    //         </li>
-    //       ))
-    //     )}
-    // </ol>
-
   );
 }
