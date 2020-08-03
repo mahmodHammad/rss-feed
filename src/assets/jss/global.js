cbc57fb667f27eb39b2e7e143c0e1961b8eeece9
Mahmoud Hammad <hodaman2012@gmail.com>
2020-08-03 22:05:48 +0200

    fix pagination style

--- src/assets/jss/global.js
+++ src/assets/jss/global.js
@@ -1,26 +1,3 @@
-/*!
-
- =========================================================
- * Material Dashboard React - v1.9.0 based on Material Dashboard - v1.2.0
- =========================================================
-
- * Product Page: http://www.creative-tim.com/product/material-dashboard-react
- * Copyright 2020 Creative Tim (http://www.creative-tim.com)
- * Licensed under MIT (https://github.com/creativetimofficial/material-dashboard-react/blob/master/LICENSE.md)
-
- =========================================================
-
- * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
- */
-
-// ##############################
-// // // Function that converts from hex color to rgb color
-// // // Example: input = #9c27b0 => output = 156, 39, 176
-// // // Example: input = 9c27b0 => output = 156, 39, 176
-// // // Example: input = #999 => output = 153, 153, 153
-// // // Example: input = 999 => output = 153, 153, 153
-// #############################
 const hexToRgb = input => {
     input = input + "";
     input = input.replace("#", "");
--- src/components/Pagination/Pagination.js
+++ src/components/Pagination/Pagination.js
@@ -1,25 +1,7 @@
 import React from "react";
 
-import { makeStyles } from '@material-ui/core/styles';
 import Pagination from '@material-ui/lab/Pagination';
 
-const useStyles = makeStyles((theme) => ({
-    root: {
-        '& > *': {
-            marginTop: theme.spacing(2),
-        },
-        margin: "auto",
-        textAlign: "center",
-    }
-
-}));
-
 export default function PaginationRounded({ handlePagination, count }) {
-    const classes = useStyles();
-
-    return (
-        <div className={classes.root}>
-            <Pagination count={count} variant="outlined" shape="rounded" onChange={handlePagination} />
-        </div>
-    );
+    return (<Pagination count={count} variant="outlined" shape="rounded" onChange={handlePagination} />);
 }
